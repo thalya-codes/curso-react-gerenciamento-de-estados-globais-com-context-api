2d45533b13f36afa6706d7f32cac735d266af0f5
Thalya Stéffany <thalyacoiimbra@gmail.com>
2023-06-20 10:36:32 -0300

    adiciona context no componente Feira

--- src/pages/Feira/index.js
+++ src/pages/Feira/index.js
@@ -1,37 +1,42 @@
-import {
-    Container,
-    Header,
-    Lista,
-  } from './styles';
-  import feira from './feira.json';
-  import Produto from '../../components/Produto';
-  import NavBar from './Navbar';
+import { useContext } from 'react';
+import { UsuarioContext } from "../../common/contexts/Usuario";
+import { Container, Header, Lista } from './styles';
+import feira from './feira.json';
+import Produto from '../../components/Produto';
+import NavBar from './Navbar';
   
-  
-  function Feira() {
-    return (
-      <Container>
-        <NavBar />
-        <Header>
-          <div>
-            <h2> Olá!</h2>
-            <h3> Saldo: R$</h3>
-          </div>
-          <p>Encontre os melhores produtos orgânicos!</p>
-        </Header>
-        <Lista>
-          <h2>
-            Produtos:
-          </h2>
-          {feira.map(produto => (
-            <Produto
-              {...produto}
-              key={produto.id}
-            />
-          ))}
-        </Lista>
-      </Container>
-    )
-  }
-  
-  export default Feira;
+function Feira() {
+  const { nome, saldo } = useContext(UsuarioContext);
+
+  return (
+    <Container>
+      <NavBar />
+      <Header>
+        <div>
+          <h2> Olá! {nome}</h2>
+          <h3> Saldo: R$ {saldo}</h3>
+        </div>
+        <p>Encontre os melhores produtos orgânicos!</p>
+      </Header>
+      <Lista>
+        <h2>
+          Produtos:
+        </h2>
+
+        {
+          feira.map(produto => (
+            <Produto {...produto}/>
+          ))
+        }
+        {/* {feira.map(produto => (
+          <Produto
+            {...produto}
+            key={produto.id}
+          />
+        ))} */}
+      </Lista>
+    </Container>
+  )
+}
+
+export default Feira;
